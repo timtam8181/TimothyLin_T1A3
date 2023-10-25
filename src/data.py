802a03420b2efe98ed5a0062fc3c2d4236d39435
timtam8181 <14898@coderacademy.edu.au>
2023-10-25 22:37:22 +1100

    researched and added in sector averages

--- src/data.py
+++ src/data.py
@@ -1,6 +1,17 @@
 import math
 import pyfiglet as pyg
 
+def sector_averages(sector):
+    sector_averages = {
+        'healthcare': {'pe_ratio': 25, 'debt_to_equity': 0.6},
+        'finance': {'pe_ratio': 10, 'debt_to_equity': 0.6},
+        'energy': {'pe_ratio': 14, 'debt_to_equity': 0.5},
+        'real_estate': {'pe_ratio': 11, 'debt_to_equity': 0.3},
+        'retail': {'pe_ratio': 12, 'debt_to_equity': 0.7},
+        'tech': {'pe_ratio': 13, 'debt_to_equity': 1},      
+
+    } 
+
 def greeting():
     welcome = pyg.figlet_format("Welcome to stock analysis!")
     print(welcome)
@@ -22,9 +33,15 @@ def stock_data():
     debt_to_equity = liabilities / (assets - liabilities) if assets != 0 else 0
     free_cash_flow = operating_cash_flow - capital_expenditure
 
+    data = {
+        'pe_ratio': pe_ratio,
+        'debt_to_equity': debt_to_equity,
+        'free_cash_flow': free_cash_flow
+    }
 
-def stock_analysis():
-    pass
+    print(f"Here are the company's metrics for your stock: {data}")
+
+
 
 greeting()
 stock_data()
